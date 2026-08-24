22c5ecc8e805de308aeaf16fe9c6468717669f85
swarag-reddy <reddyswarag02@gmail.com>
2026-08-24 08:33:11 -0700

    Add Docker multi - service architecture

--- alembic/env.py
+++ alembic/env.py
@@ -1,5 +1,6 @@
 from logging.config import fileConfig
 
+from config import DATABASE_URL
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
@@ -11,6 +12,7 @@ import models
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
